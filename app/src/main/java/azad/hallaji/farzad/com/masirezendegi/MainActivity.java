a2eeaf4295362ca957b69d6329cd954b0229bc71
fayzad <farzadhallaji@yahoo.com>
2017-04-05 07:02:58 +0430

    check web servis

--- app/src/main/java/azad/hallaji/farzad/com/masirezendegi/MainActivity.java
+++ app/src/main/java/azad/hallaji/farzad/com/masirezendegi/MainActivity.java
@@ -21,25 +21,35 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import azad.hallaji.farzad.com.masirezendegi.internet.HttpManager;
 import azad.hallaji.farzad.com.masirezendegi.internet.RequestPackage;
 import azad.hallaji.farzad.com.masirezendegi.model.GlobalVar;
+import azad.hallaji.farzad.com.masirezendegi.model.Question;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    String imei,wifimac,uniqueid,androidid,simno,operator,brand,model
-            ,android_sdk,android_version,height,width,device_size,version,os="";
+    String imei, wifimac, uniqueid, androidid, simno, operator, brand, model, android_sdk, android_version, height, width, device_size, version, os = "";
 
 
     TextView textView;
@@ -52,19 +62,18 @@ public class MainActivity extends AppCompatActivity {
 
         initial();
 
-        textView = (TextView)findViewById(R.id.idLoadingTextview);
-        progressBar=(ProgressBar)findViewById(R.id.loadprogs);
+        textView = (TextView) findViewById(R.id.idLoadingTextview);
+        progressBar = (ProgressBar) findViewById(R.id.loadprogs);
         progressBar.setVisibility(View.GONE);
-
-
 
 
         if (isOnline()) {
 
-            Toast.makeText(getApplicationContext(), asad(), Toast.LENGTH_LONG).show();
-
 
             progressBar.setVisibility(View.VISIBLE);
+
+            Toast.makeText(getApplicationContext(), "jeddi", Toast.LENGTH_LONG).show();
+            requestData();
 
         } else {
             Toast.makeText(getApplicationContext(), "Network isn't available", Toast.LENGTH_LONG).show();
@@ -74,107 +83,57 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private String asad() {
-
-        BufferedReader reader = null;
-
-        try {
-            URL url = new URL("http://telyar.dmedia.ir/webservice/check");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestProperty("magent",  imei+"/"+wifimac+"/"+uniqueid+"/"+androidid+"/"+simno+"/"+operator+"/"+brand+"/"+model
-                    +"/"+android_sdk+"/"+android_version+"/"+height+"/"+width+"/"+device_size+"/"+version+"/"+os);
-
-            con.setDoOutput(true);
-            OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
-            writer.flush();
-
-            StringBuilder sb = new StringBuilder();
-            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            Toast.makeText(getApplicationContext(), sb.toString() , Toast.LENGTH_LONG).show();
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-
-
-            return sb.toString();
-
-        } catch (MalformedURLException e) {
-            //e.printStackTrace();
-            return "catch1";
-
-        } catch (IOException e) {
-            //e.printStackTrace();
-            return "catch3";
-
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    //e.printStackTrace();
-                    return "catch2";
-                }
-            }
-        }
-
-    }
-
 
     @SuppressLint("HardwareIds")
     private void initial() {
 
-        device_size="";
-        version="1.0";
-        os= "android";
+        device_size = "";
+        version = "1.0";
+        os = "android";
 
-        try{
-            TelephonyManager mngr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-            imei =mngr.getDeviceId();
-            operator=mngr.getNetworkOperatorName();
-            simno= mngr.getLine1Number();
-        }catch (Exception e){
-            Log.i("errorMainActivity",e.toString());
+        try {
+            TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            imei = mngr.getDeviceId();
+            operator = mngr.getNetworkOperatorName();
+            simno = mngr.getLine1Number();
+        } catch (Exception e) {
+            Log.i("errorMainActivity", e.toString());
         }
 
-        try{
+        try {
             WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             WifiInfo info = manager.getConnectionInfo();
             wifimac = info.getMacAddress();
-        }catch (Exception e){
-            Log.i("errorMainActivity",e.toString());
+        } catch (Exception e) {
+            Log.i("errorMainActivity", e.toString());
         }
 
-        try{
+        try {
 
-            androidid= Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-            uniqueid= Settings.Secure.getString(this.getContentResolver(), Settings.Secure.NAME);
-        }catch (Exception e){
-            Log.i("errorMainActivity",e.toString());
+            androidid = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+            uniqueid = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.NAME);
+        } catch (Exception e) {
+            Log.i("errorMainActivity", e.toString());
         }
 
-        try{
+        try {
 
-            androidid= Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-            uniqueid= Settings.Secure.getString(this.getContentResolver(), Settings.Secure.NAME);
-        }catch (Exception e){
-            Log.i("err",e.toString());
+            androidid = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+            uniqueid = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.NAME);
+        } catch (Exception e) {
+            Log.i("err", e.toString());
         }
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        height=String.valueOf(displayMetrics.heightPixels);
-        width=String.valueOf(displayMetrics.widthPixels);
+        height = String.valueOf(displayMetrics.heightPixels);
+        width = String.valueOf(displayMetrics.widthPixels);
 
-        android_version= Build.VERSION.RELEASE;
-        android_sdk=String.valueOf(Build.VERSION.SDK_INT);
+        android_version = Build.VERSION.RELEASE;
+        android_sdk = String.valueOf(Build.VERSION.SDK_INT);
 
-        model= android.os.Build.MODEL;
+        model = android.os.Build.MODEL;
         brand = Build.MANUFACTURER;
-
-        String source="magent"+":"+imei+"/"+wifimac+"/"+uniqueid+"/"+androidid+"/"+simno+"/"+operator+"/"+brand+"/"+model
-                +"/"+android_sdk+"/"+android_version+"/"+height+"/"+width+"/"+device_size+"/"+version+"/"+os;
 
 
     }
@@ -189,4 +148,57 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+
+    private void requestData() {
+
+        RequestPackage p = new RequestPackage();
+        p.setMethod("POST");
+        p.setUri("http://telyar.dmedia.ir/webservice/check");
+
+        String source =  imei + "/" + wifimac + "/" + uniqueid + "/" + androidid + "/" + simno + "/" + operator + "/" + brand + "/" + model
+                + "/" + android_sdk + "/" + android_version + "/" + height + "/" + width + "/" + device_size + "/" + version + "/" + os;
+        Toast.makeText(getApplicationContext(), "0"+source+"0", Toast.LENGTH_LONG).show();
+
+
+        p.setParam("Magent", source);
+
+
+
+        LoginAsyncTask task = new LoginAsyncTask();
+        task.execute(p);
+
+    }
+
+
+    private class LoginAsyncTask extends AsyncTask<RequestPackage, String, String> {
+
+
+        @Override
+        protected String doInBackground(RequestPackage... params) {
+            String content = HttpManager.getData(params[0]);
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "0"+result+"0", Toast.LENGTH_LONG).show();
+
+
+            /*List<Question> templist=new ArrayList<>();
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(result);
+
+                progressBar.setVisibility(View.GONE);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }*/
+
+        }
+
+    }
 }
