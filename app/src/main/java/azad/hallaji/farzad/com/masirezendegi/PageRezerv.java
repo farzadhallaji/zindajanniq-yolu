ae68c3759b6d9d17a477cb740b31f5da002d1b67
fayzad <farzadhallaji@yahoo.com>
2017-05-06 02:20:19 +0430

    takmil6

--- app/src/main/java/azad/hallaji/farzad/com/masirezendegi/PageRezerv.java
+++ app/src/main/java/azad/hallaji/farzad/com/masirezendegi/PageRezerv.java
@@ -4,9 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -15,6 +23,12 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -24,14 +38,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import azad.hallaji.farzad.com.masirezendegi.helper.AdapterRezerv;
+import azad.hallaji.farzad.com.masirezendegi.internet.HttpManager;
+import azad.hallaji.farzad.com.masirezendegi.internet.RequestPackage;
 import azad.hallaji.farzad.com.masirezendegi.model.GlobalVar;
 import azad.hallaji.farzad.com.masirezendegi.model.Rezervable;
 import cz.msebera.android.httpclient.Header;
 
-public class PageRezerv extends AppCompatActivity {
+public class PageRezerv extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener  {
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
@@ -43,6 +62,45 @@ public class PageRezerv extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_rezerv);
 
+
+
+        ImageView imageView1 = (ImageView) findViewById(R.id.backButton);
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PageRezerv.this , ExplainMoshaver.class);
+                startActivity(intent);
+            }
+        });
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ImageView imageView = (ImageView) findViewById(R.id.menuButton);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.END);
+            }
+        });
+
+        if(GlobalVar.getUserID().equals("100")){
+            Menu nav_Menu = navigationView.getMenu();
+            nav_Menu.findItem(R.id.nav_marakez).setVisible(true);
+            nav_Menu.findItem(R.id.nav_profile).setVisible(false);
+            nav_Menu.findItem(R.id.nav_login).setVisible(true);
+            nav_Menu.findItem(R.id.nav_moshaverin).setVisible(true);
+            nav_Menu.findItem(R.id.nav_porseshha).setVisible(true);
+            nav_Menu.findItem(R.id.nav_logout).setVisible(false);
+        }else{
+            Menu nav_Menu = navigationView.getMenu();
+            nav_Menu.findItem(R.id.nav_marakez).setVisible(true);
+            nav_Menu.findItem(R.id.nav_profile).setVisible(true);
+            nav_Menu.findItem(R.id.nav_login).setVisible(false);
+            nav_Menu.findItem(R.id.nav_moshaverin).setVisible(true);
+            nav_Menu.findItem(R.id.nav_porseshha).setVisible(true);
+            nav_Menu.findItem(R.id.nav_logout).setVisible(true);
+        }
 
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -85,7 +143,7 @@ public class PageRezerv extends AppCompatActivity {
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 try {
 
-                    List<Rezervable> rezervableList=new ArrayList<>();
+                    final List<Rezervable> rezervableList=new ArrayList<>();
                     Log.i("wertyuio",new String(responseBody));
                     JSONArray jsonArray = new JSONArray(new String(responseBody));
 
@@ -124,6 +182,15 @@ public class PageRezerv extends AppCompatActivity {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                            /*‫‪Reserve‬‬*/
+                            /*‫‪Output‬‬ ‫‪:‬‬ ‫‪Message‬‬ ‫‪,‬‬ ‫‪ReserveID‬‬*/
+                            String placeid=rezervableList.get(position).getPlaceID();
+                            String adviserid=rezervableList.get(position).getAdviserID();
+                            String rid=rezervableList.get(position).getRID();
+                            //Reserve(placeid,adviserid,rid);
+                            Reserve2(placeid,adviserid,rid);
+                            //requestData(placeid,adviserid,rid);
+
                         }
                     });
 
@@ -138,6 +205,77 @@ public class PageRezerv extends AppCompatActivity {
         });
     }
 
+    private void Reserve(String placeID, String adviserID, String rid) {
+
+        RequestParams params = new RequestParams();
+        //‫‪Input‬‬ ‫‪:‬‬ ‫‪deviceid‬‬ ‫‪,‬‬ ‫‪placeid‬‬ ‫‪,‬‬ ‫‪adviserid‬‬ ‫‪,‬‬ ‫‪userid‬‬ ‫‪,‬‬ ‫‪rid‬‬
+        params.put("adviserid",adviserID); //Add the data you'd like to send to the server.
+        params.put("rid",rid); //Add the data you'd like to send to the server.
+        params.put("placeid", placeID);
+        params.put("userid", GlobalVar.getUserID());
+        params.put("deviceid", GlobalVar.getDeviceID());
+        /*params.put("adviserid","177"); //Add the data you'd like to send to the server.
+        params.put("rid","1"); //Add the data you'd like to send to the server.
+        params.put("placeid", "2");
+        params.put("userid", "2");
+        params.put("deviceid", "3");*/
+        client.post("http://telyar.dmedia.ir/webservice/Reserve", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+
+                    final List<Rezervable> rezervableList=new ArrayList<>();
+                    Log.i("wertyuio",new String(responseBody));
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.i("aassdfghjuytrew","1");
+            }
+        });
+
+    }
+    private void Reserve2(final String placeID, final String adviserID, final String rid) {
+
+
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+
+        String url = "http://telyar.dmedia.ir/webservice/Reserve";
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+                Log.i("ExplainMoshaver",response);
+                //Toast.makeText(getApplicationContext(), response , Toast.LENGTH_LONG).show();
+
+
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                //Log.i("asasasasasasa",adviseridm+"/"+GlobalVar.getDeviceID());
+                params.put("adviserid",adviserID); //Add the data you'd like to send to the server.
+                params.put("rid",rid); //Add the data you'd like to send to the server.
+                params.put("placeid", placeID);
+                params.put("userid", GlobalVar.getUserID());
+                params.put("deviceid", GlobalVar.getDeviceID());
+                return params;
+            }
+        };
+
+        MyRequestQueue.add(MyStringRequest);
+
+    }
 
 
     private boolean isOnline() {
@@ -148,5 +286,69 @@ public class PageRezerv extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+
+    private void requestData(String placeID, String adviserID, String rid) {
+
+        RequestPackage params = new RequestPackage();
+        params.setMethod("POST");
+        params.setUri("http://telyar.dmedia.ir/webservice/Reserve/");
+
+        params.setParam("adviserid",adviserID); //Add the data you'd like to send to the server.
+        params.setParam("rid",rid); //Add the data you'd like to send to the server.
+        params.setParam("placeid", placeID);
+        params.setParam("userid", GlobalVar.getUserID());
+        params.setParam("deviceid", GlobalVar.getDeviceID());
+
+        LoginAsyncTask task = new LoginAsyncTask();
+        task.execute(params);
+
+    }
+
+
+    private class LoginAsyncTask extends AsyncTask<RequestPackage, String, String> {
+
+
+        @Override
+        protected String doInBackground(RequestPackage... params) {
+            String content = HttpManager.getData(params[0]);
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            Log.i("saasasa","asdf") ;
+            Log.i("saasasa",result) ;
+
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_marakez) {
+            startActivity(new Intent(this , PageMarakez.class));
+        } else if (id == R.id.nav_profile) {
+            startActivity(new Intent(this , PageVirayesh.class));
+        } else if (id == R.id.nav_login) {
+            startActivity(new Intent(this , PageLogin.class));
+        } else if (id == R.id.nav_moshaverin) {
+            startActivity(new Intent(this , PageMoshaverin.class));
+        } else if (id == R.id.nav_porseshha) {
+            startActivity(new Intent(this , PagePorseshha.class));
+        } else if (id == R.id.nav_logout){
+            startActivity(new Intent(this , PageLogout.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.END);
+        return true;
+
     }
 }
